f58a7915616f37e7c2fdedf8587425f6ba0f7473
borges <jborgespais@gmail.com>
2023-03-24 11:33:40 -0300

    List dict to DataFrame

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,6 +1,7 @@
 from app.index import (
+    list_dict_to_dataframe,
     request_get_data,
-    list_str_dict_to_list_dict
+    str_dict_to_list_dict
 )
 
 
@@ -14,11 +15,25 @@ def test_request_get_data():
         assert False
 
 
-def test_list_str_dict_to_list_dict():
+def test_str_dict_to_list_dict():
     str_dict = "{'env': 'prod', 'path': '/path1', 'method': 'PUT'}"
 
     try:
-        list_str_dict_to_list_dict(data=str_dict)
+        str_dict_to_list_dict(data=str_dict)
         assert True
     except:
         assert False
+
+
+def test_list_dict_to_dataframe():
+    list_dict = [
+        {"env": "prod", "path": "/path1", "method": "PUT"},
+        {"env": "prod", "path": "/path1", "method": "PUT"},
+        {"env": "prod", "path": "/path1", "method": "PUT"}
+    ]
+
+    try:
+        list_dict_to_dataframe(data=list_dict)
+        assert True
+    except:
+        assert  False
