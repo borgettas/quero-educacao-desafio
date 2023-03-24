921f29b78a0b0f0fed8a83fc251fee4941312b6d
borges <jborgespais@gmail.com>
2023-03-24 11:18:11 -0300

    Add transform list str to list dict

--- app/index.py
+++ app/index.py
@@ -2,8 +2,6 @@ import json
 import pandas as pd
 import requests
 
-from urllib.request import urlopen
-
 
 DATA_URL = "https://s3.amazonaws.com/gupy5/production/companies/41683/emails/1679437007669/8408fe80-c80f-11ed-80da-7d3cd1fee4b4/log.txt"
 
@@ -20,8 +18,23 @@ def request_get_data(url: str):
         print(f"❌ Request Error - {response.status_code}")
 
 
+def list_str_dict_to_list_dict(data: str) -> list:
+
+    list_dict = []
+
+    splited_data = data.split("\n")
+    
+    for item in splited_data:
+        if item != '':
+            json_object = json.loads(item.replace("'", "\""))
+            list_dict.append(json_object)
+    
+    return list_dict
+
+
 def main():
     data = request_get_data(DATA_URL)
+    splited_data = list_str_dict_to_list_dict(data=data)
 
 
 if  __name__ == "__main__":
